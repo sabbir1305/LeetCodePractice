d3a67abc4973f42867fe7365c405ef698fab04f0
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-06-17 23:30:24 +0600

    Find Duplicate

---
+++ ContainsDuplicate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ContainsDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            var nums = new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+            if (solution.ContainsDuplicate(nums)){
+                Console.WriteLine("Contains Duplicate");
+            }
+            else
+            {
+                Console.WriteLine("Uninque List");
+
+            }
+        }
+    }
+}
---
+++ ContainsDuplicate/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainsDuplicate
+{
+    class Solution
+    {
+        //public bool ContainsDuplicate(int[] nums)
+        //{
+        //    var hashtable = new Hashtable();
+        //    foreach (var item in nums)
+        //    {
+        //        if (hashtable.ContainsKey(item))
+        //            return true;
+        //        hashtable.Add(item, item);
+        //    }
+        //    return false;
+
+        //}
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var hasSet = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                if (hasSet.Contains(item))
+                    return true;
+                hasSet.Add(item);
+            }
+            return false;
+
+        }
+    }
+}
