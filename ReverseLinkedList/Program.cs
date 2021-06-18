bbb06e963dd2bea0942fbdc8198fdc157319d76d
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-06-19 00:00:06 +0600

    ReverseLinkedList

---
+++ ReverseLinkedList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ReverseLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nodes = new ListNode();
+            nodes.val = 1;
+            nodes.next = new ListNode();
+            nodes.next.val = 2;
+            Solution solution = new Solution();
+
+            
+           var result = solution.ReverseList(nodes);
+            Display(result);
+
+
+
+        }
+
+        public static void Display(ListNode head)
+        {
+            if (head == null )
+            {
+                return ;
+            }
+            Console.Write(head.val+ "==>");
+
+            Display(head.next);
+
+
+           
+        }
+    }
+}
---
+++ ReverseLinkedList/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseLinkedList
+{
+    class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode currentNode = head;
+            ListNode temp = null;
+
+            if (head == null) { return null; }
+            while (currentNode.next != null)
+            {
+                temp = currentNode.next;
+                currentNode.next = temp.next;
+                temp.next = head;
+                head = temp;
+            }
+            return head;
+
+
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+
+   
+
+
+
+
