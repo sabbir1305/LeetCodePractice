c1767ffd76119c83265a03496d0bfed73d061740
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-06-19 23:38:48 +0600

    Two sum II

---
+++ TwoSum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var numbers = new int[] { 5, 25, 75 };
+            Solution solution = new Solution();
+            var result = solution.TwoSum(numbers,100);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ TwoSum/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    class Solution
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            var resultIndex = new int[2];
+
+            var end = numbers.Length - 1;
+            int start = 0;
+
+            while (start < end)
+            {
+                int tempTarget = numbers[start] + numbers[end];
+                if (tempTarget < target)
+                {
+                    start += 1;
+                }
+                else if (tempTarget > target)
+                {
+                    end -= 1;
+                }
+                else
+                {
+                    resultIndex[0] = start + 1;
+                    resultIndex[1] = end + 1;
+
+                    return resultIndex;
+                }
+            }
+
+
+
+
+            return resultIndex;
+        }
+    }
+
+}
