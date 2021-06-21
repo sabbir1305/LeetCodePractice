3aa8f93edfdb7c0e298d68479d200f1ac13fef60
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-06-21 22:58:47 +0600

    Palindrome linked list

---
+++ PalindromeLinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PalindromeLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1,2,2,1
+            ListNode listNode = new ListNode(1);
+            listNode.next = new ListNode(2);
+            listNode.next.next = new ListNode(2);
+            listNode.next.next.next = new ListNode(1);
+            var sln = new Solution();
+            if (sln.IsPalindrome(listNode))
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+           
+        }
+    }
+}
---
+++ PalindromeLinkedList/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeLinkedList
+{
+    class Solution
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while(fast!=null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            slow = Reverse(slow);
+            fast = head;
+            while (slow != null)
+            {
+                if (slow.val != fast.val)
+                {
+                    return false;
+                }
+
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            return true;
+        }
+
+        private ListNode Reverse(ListNode head)
+        {
+            ListNode prev = null;
+            while (head != null)
+            {
+                ListNode next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+            return prev;
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
