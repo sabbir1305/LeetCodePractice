b34e527e9b55578d06d06b45543f474af1c9c269
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2024-01-27 21:06:06 +0600

    solved 383 using dictionary

---
+++ 383RansomNote/Program.cs
@@ -0,0 +1,59 @@
+﻿//Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+//Each letter in magazine can only be used once in ransomNote.
+
+
+
+//Example 1:
+
+//Input: ransomNote = "a", magazine = "b"
+//Output: false
+
+//Example 2:
+
+//Input: ransomNote = "aa", magazine = "ab"
+//Output: false
+
+//Example 3:
+
+//Input: ransomNote = "aa", magazine = "aab"
+//Output: true
+
+
+
+
+//Constraints:
+
+//1 <= ransomNote.length, magazine.length <= 105
+//    ransomNote and magazine consist of lowercase English letters.
+
+Console.WriteLine(CanConstruct("aaaa","aaab"));
+static bool CanConstruct(string ransomNote, string magazine)
+{
+    Dictionary<char, int> magazineChars = new Dictionary<char, int>();
+    foreach ( char c in magazine )
+    {
+        if( magazineChars.ContainsKey(c))
+        {
+            magazineChars[c] += 1;
+        }
+        else
+        {
+            magazineChars[c] = 1;
+        }
+
+    }
+
+    foreach ( char c in ransomNote )
+    {
+        if ( magazineChars.TryGetValue(c, out int count) && count > 0)
+        {
+            magazineChars[c] --;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
